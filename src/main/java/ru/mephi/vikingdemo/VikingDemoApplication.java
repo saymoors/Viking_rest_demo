0cff2339584bc2015a960805e599f7668613bd8d
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-05-05 15:19:12 +0300

    Добавление окна анализатора

--- src/main/java/ru/mephi/vikingdemo/VikingDemoApplication.java
+++ src/main/java/ru/mephi/vikingdemo/VikingDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import ru.mephi.vikingdemo.gui.VikingDesktopFrame;
 
 import javax.swing.SwingUtilities;
+import ru.mephi.vikingdemo.controller.VikingAnalysisListener;
 import ru.mephi.vikingdemo.controller.VikingListener;
 import ru.mephi.vikingdemo.service.VikingService;
 
@@ -15,15 +16,18 @@ public class VikingDemoApplication {
     public static void main(String[] args) {
         System.out.println(java.awt.GraphicsEnvironment.isHeadless());
         SpringApplication app = new SpringApplication(VikingDemoApplication.class);
-        app.setHeadless(false); // Для доступа к GUI
+        app.setHeadless(false);
 
         ConfigurableApplicationContext context = app.run(args);
 
         VikingService vikingService = context.getBean(VikingService.class);
-        VikingListener vikingListener = context.getBean(VikingListener.class);    
+        VikingListener vikingListener = context.getBean(VikingListener.class);
+        VikingAnalysisListener analysisListener = context.getBean(VikingAnalysisListener.class);
+
         SwingUtilities.invokeLater(() -> {
-            VikingDesktopFrame frame = new VikingDesktopFrame(vikingService);
+            VikingDesktopFrame frame = new VikingDesktopFrame(vikingService, vikingListener, analysisListener);
             vikingListener.setGui(frame);
+            analysisListener.setGui(frame);
             frame.setVisible(true);
         });
     }
